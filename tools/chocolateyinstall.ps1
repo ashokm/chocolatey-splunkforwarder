641544c60fda353bc1286743aec8d56f0a789e22
Ashok Manji <1902568+ashokm@users.noreply.github.com>
2019-09-17 11:46:38 +0100

    Bump version to 7.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = $env:ChocolateyPackageName
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86&platform=windows&version=7.2.3&product=universalforwarder&filename=splunkforwarder-7.2.3-06d57c595b80-x86-release.msi&wget=true' # download url, HTTPS preferred
-$url64 = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.2.3&product=universalforwarder&filename=splunkforwarder-7.2.3-06d57c595b80-x64-release.msi&wget=true' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86&platform=windows&version=7.3.0&product=universalforwarder&filename=splunkforwarder-7.3.0-657388c7a488-x86-release.msi&wget=true' # download url, HTTPS preferred
+$url64 = 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=windows&version=7.3.0&product=universalforwarder&filename=splunkforwarder-7.3.0-657388c7a488-x64-release.msi&wget=true' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
 
 $packageArgs = @{
     packageName    = $packageName
@@ -13,9 +13,9 @@ $packageArgs = @{
     url64bit       = $url64
 
     softwareName   = 'SplunkUniversalForwarder'
-    checksum       = '00468A7C83C028971BF965B454F49D7F'
+    checksum       = '1a46010d8112247a32a1fe589ec5dc54'
     checksumType   = 'md5'
-    checksum64     = 'EAADA91BCA364B0B3D37163BEB9F4100'
+    checksum64     = 'dc6aca9bcd3d23053f6de901f37ef089'
     checksumType64 = $checksumType
 
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AGREETOLICENSE=YES" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
